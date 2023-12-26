66972396717e27e14b55c4fff14c753be5b284d1
Rauf Mammadov <raufama@code.edu.az>
2023-12-26 16:57:30 +0400

    Turtle py project

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+
+drawing_board = turtle.Screen()
+
+drawing_board.bgcolor("green")
+
+drawing_board.title("Python turtle")
+
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(100)
+
+turtle.done()
