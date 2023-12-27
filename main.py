df90a7c0af0463cc47a762b60fe808fcf84ad577
Rauf Mammadov <raufama@code.edu.az>
2023-12-27 15:09:03 +0400

    kvadrat

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ drawing_board.title("Python turtle")
 
 turtle_instance = turtle.Turtle()
 
-turtle_instance.forward(100)
+for i in range(4):
+    turtle_instance.forward(50)
 
-turtle_instance2 = turtle.Turtle()
-turtle_instance2.left(90)
-turtle_instance2.forward(100)
-
+    turtle_instance.left(90)
 
 turtle.done()
